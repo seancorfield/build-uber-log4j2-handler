ab8e499d7e38714ca3df08411c5a591440250214
Sean Corfield <sean@corfield.org>
2021-09-14 10:33:20 -0700

    basic log4j2 plugins merger

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # build-uber-log4j2-handler
-A conflict handler for log4j2 plugins cache files for the tools.build uber task.
+
+A conflict handler for log4j2 plugins cache files for the `uber` task of `tools.build`.
+
+Log4j2-based libraries often have a `Log4j2Plugins.dat` file, containing a cache of
+formatting plugins. If you build an uberjar containing more then one of these files,
+you need this conflict handler in order to merge those files correctly.
+
+> Note: requires a version of `tools.build` that supports `:conflict-handlers`!
+
+## usage
+
+Add the following dependency to your `:build` alias:
+
+```clojure
+  io.github.seancorfield/build-uber-log4j2-handler {:git/tag "v0.1.0" :git/sha "..."}
+```
+
+In your `build.clj` script, require the handler:
+
+```clojure
+(ns build
+  (:require [clojure.tools.build.api :as b]
+            [org.corfield.log4j2-conflict-handler
+              :refer [log4j2-conflict-handler]])
+```
+
+and then pass it to the `tools.build` `uber` task:
+
+```clojure
+  (b/uber {... :conflict-handlers log4j2-conflict-handler ...})
+```
+
+## license
+
+Copyright © 2021 Sean Corfield
+
+Distributed under the Apache Software License version 2.0.
---
+++ src/org/corfield/log4j2_conflict_handler.clj
@@ -0,0 +1,35 @@
+;; copyright (c) 2021 sean corfield, all rights reserved
+
+(ns org.corfield.log4j2-conflict-handler
+  "Provides a conflict handler for use with tools.build.
+
+  (ns build
+    (:require [clojure.tools.build.api :as b]
+              [org.corfield.log4j2-conflict-handler
+               :refer [log4j2-conflict-handler]])
+
+  (b/uber {... :conflict-handlers log4j2-conflict-handler ...})"
+  (:require [clojure.java.io :as io])
+  (:import (java.io File InputStream)
+           (java.util Collection Enumeration Vector)
+           (org.apache.logging.log4j.core.config.plugins.processor PluginCache)))
+
+(set! *warn-on-reflection* true)
+
+(defn log4j2-plugin-merger
+  "Conflict handler for clojure.tools.build.api/uber."
+  [{:keys [^InputStream in ^File existing]}]
+  (let [->enumeration (fn ^Enumeration [^Collection coll]
+                        (.elements (Vector. coll)))
+        cache (PluginCache.)
+        temp  (File/createTempFile "plugins" ".dat")]
+    (io/copy in temp :buffer-size 4096)
+    (.loadCacheFiles cache (->enumeration (map io/as-url [existing temp])))
+    (with-open [os (io/output-stream existing)]
+      (.writeCache cache os))
+    ;; nothing to write, no state change either:
+    nil))
+
+(def log4j2-conflict-handler
+  {#"^META-INF/org/apache/logging/log4j/core/config/plugins/Log4j2Plugins.dat$"
+   log4j2-plugin-merger})
